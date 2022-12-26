29e4d80de7ef5f6ce2a26bdafb6a9b0f82f34894
Carlos Eduardo Jaramillo Londoño <cejl397@gmail.com>
2022-12-26 16:14:35 +0100

    [FEAT] Add axum default config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    response::Html,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/", get(hello_world));
+
+        println!("Playground: http://localhost:3000");
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+
+async fn hello_world() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
